4cb4565c00aab7df2ff535750c749fac77a2fcff
sebathefox <seba4928@edu.sde.dk>
2019-01-03 15:32:19 +0100

    First commit

---
+++ MailDesk.Core/View/Mail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MailDesk.Core.View
+{
+    public class Mail : ListBoxItem
+    {
+        protected Label mailTitle;
+        protected Label sender;
+
+        protected ContentControl content;
+
+        public Mail()
+        {
+
+            mailTitle = new Label();
+            sender = new Label();
+            mailTitle.Content = "Mail";
+            sender.Content = "Me";
+
+            StackPanel panel = new StackPanel();
+
+            panel.Children.Add(mailTitle);
+            panel.Children.Add(sender);
+            this.Content = panel;
+        }
+
+        public void AddContent(object content)
+        {
+            //this.Content
+        }
+    }
+}
