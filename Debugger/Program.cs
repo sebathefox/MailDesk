0e1d439af1fd5948d526c627760e1b62bd44c036
sebathefox <seba4928@edu.sde.dk>
2019-01-05 13:46:32 +0100

    Added basic features to the backend

---
+++ Debugger/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImapX;
+using ImapX.Collections;
+using ImapX.Enums;
+using MailDesk.Core;
+
+namespace Debugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Imap imap = new Imap("imap.gmail.com", 993, true);
+
+            imap.Connect();
+
+            imap.Login("skrivtilsebastian99@gmail.com", "abebaudel");
+            imap.OnMessageReceived += OnReceived;
+            
+            imap.FetchMails();                
+            while (true)
+            {
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void OnReceived(object sender, IdleEventArgs args)
+        {
+            foreach (Message message in args.Messages)
+            {
+                Console.WriteLine("Subject: " + message.Subject);
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.WriteLine(message.Body.Text);
+            }
+        }
+    }
+}
---
+++ MailDesk.Core/Imap.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImapX;
+using ImapX.Collections;
+
+namespace MailDesk.Core
+{
+    public class Imap
+    {
+        private ImapClient client;
+
+        public event EventHandler<IdleEventArgs> OnMessageReceived;
+
+        public Imap(string host, int port, bool ssl)
+        {
+            client = new ImapClient(host, port, ssl);
+            client.OnNewMessagesArrived += (sender, args) => { OnMessageReceived?.Invoke(sender, args);};
+        }
+
+        public bool Connect()
+        {
+            return client.Connect();
+        }
+
+        public bool Login(string username, string password)
+        {
+            return client.Login(username, password);
+        }
+
+        public FolderCollection GetFolders()
+        {
+            return client.Folders;
+        }
+
+        public Folder GetFolder(string name)
+        {
+            return client.Folders[name];
+        }
+
+        public IEnumerable<Folder> WalkFoldersRecursive(FolderCollection folders)
+        {
+            foreach (Folder folder in folders)
+            {
+                if (folder.HasChildren)
+                {
+
+                    foreach (Folder item in WalkFoldersRecursive(folder.SubFolders))
+                    {
+                        yield return item;
+                    }
+
+                }
+                yield return folder;
+            }
+        }
+
+        public void FetchMails()
+        {
+            foreach (Message message in GetFolder("INBOX").Search("UNSEEN"))
+            {
+                Console.WriteLine(message.Body.Text);
+            }
+        }
+    }
+}
